80144b3c09c79a84fb20e405745b20564c86c9f9
chisan fung <tomathosauce@users.noreply.github.com>
2024-12-25 15:23:19 +0000

    build

---
+++ build/precache-manifest.72e9a80ccedea87c08e58b5e753dfdfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c9d63592cea1cc1973380615084c1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce48ff89b856153d0703",
+    "url": "/static/css/main.93d71d7a.chunk.css"
+  },
+  {
+    "revision": "77dfdccf620049fee69f",
+    "url": "/static/js/2.03a2a4bb.chunk.js"
+  },
+  {
+    "revision": "ce48ff89b856153d0703",
+    "url": "/static/js/main.607448b0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
